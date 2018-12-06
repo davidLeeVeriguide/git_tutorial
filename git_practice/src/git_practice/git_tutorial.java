cbd0102eb0756d552d38358ca622647243a8a2d2
davidLeeVeriguide <david@veriguide.org>
2018-12-06 17:01:48 +0800

    There is a new branch called subroutine and I would like to commit some new code to the master branch

--- git_practice/src/git_practice/git_tutorial.java
+++ git_practice/src/git_practice/git_tutorial.java
@@ -6,6 +6,9 @@ public class git_tutorial {
 		System.out.println("Hello World! 456888");
 		System.out.println("Hello World! 123");
 		System.out.println("yoyoyo");
+		
+		// I try to add something here after a branch is created
+		System.out.println("A branch called subroutine is created!");
 	}
 	
 }
