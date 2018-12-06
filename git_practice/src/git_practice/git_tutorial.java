b50c84cfc79782d131d9f912a54869c8f3dbccb5
davidLeeVeriguide <david@veriguide.org>
2018-12-06 11:32:40 +0800

    merged the two commits togther as one file with two print lines

--- git_practice/src/git_practice/git_tutorial.java
+++ git_practice/src/git_practice/git_tutorial.java
@@ -3,11 +3,8 @@ package git_practice;
 public class git_tutorial {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello World! 456");
-=======
 		System.out.println("Hello World! 123");
->>>>>>> branch 'master' of https://github.com/davidLeeVeriguide/git_tutorial.git
 	}
 	
 }
