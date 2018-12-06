2c68cba43d81b0f609fc059b9e2728dde019168f
davidLeeVeriguide <david@veriguide.org>
2018-12-06 11:07:39 +0800

    Added 456 after Hello world

--- git_practice/src/git_practice/git_tutorial.java
+++ git_practice/src/git_practice/git_tutorial.java
@@ -3,7 +3,7 @@ package git_practice;
 public class git_tutorial {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! 456");
 	}
 	
 }
