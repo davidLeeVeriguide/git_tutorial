5d1b6d8c509d1806fb656f7025d641664e0736b6
davidLeeVeriguide <david@veriguide.org>
2018-12-06 11:43:13 +0800

    changed 456 to 456888 added yoyoyo

--- git_practice/src/git_practice/git_tutorial.java
+++ git_practice/src/git_practice/git_tutorial.java
@@ -3,8 +3,9 @@ package git_practice;
 public class git_tutorial {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World! 456");
+		System.out.println("Hello World! 456888");
 		System.out.println("Hello World! 123");
+		System.out.println("yoyoyo");
 	}
 	
 }
